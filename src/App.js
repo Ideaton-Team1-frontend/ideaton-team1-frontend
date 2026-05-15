f6344eef6d6962c9383f5e5ee9765b8b31dc26a7
cy123-coder <wolgugu@gmail.com>
2026-05-15 11:25:37 +0900

    Feat: 흰박스 컴포넌트, result페이지 작업중

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GlobalStyles } from "./styles";
 import OnBoarding from "./pages/OnBoarding"
 import MainPage from "./pages/MainPage"
+import ResultPage from "./pages/ResultPage"
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element ={<OnBoarding/>}/>
-        <Route path="/MainPage" element ={<MainPage/>}/>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element ={<ResultPage/>}/>
+          <Route path="/MainPage" element ={<MainPage/>}/>
+          <Route path="/ResultPage" element ={<ResultPage/>}/>
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
