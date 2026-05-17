bf5b514e29dbd956b2df1074334bbc102eaf803f
cy123-coder <wolgugu@gmail.com>
2026-05-18 00:47:30 +0900

    흠

--- src/pages/KidData.jsx
+++ src/pages/KidData.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DatePicker from "react-datepicker";
