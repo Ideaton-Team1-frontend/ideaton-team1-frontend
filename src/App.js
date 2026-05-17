2a54eec9c28cfee164b409abbae66b18a4f671f9
cy123-coder <wolgugu@gmail.com>
2026-05-18 01:52:10 +0900

    아

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { GlobalStyles } from "./styles";
 import OnBoarding from "./pages/OnBoarding";
 import MainPage from "./pages/MainPage";
 import ResultPage from "./pages/ResultPage";
-import Camera from "./pages/Camera";
 import KidData from "./pages/KidData";
 import ImageChoice from "./pages/ImageChoice";
-import Testpost from "./pages/Testpost";
+
 
 function App() {
   return (
@@ -18,10 +17,8 @@ function App() {
           <Route path="/" element={<OnBoarding />} />
           <Route path="/mainPage" element={<MainPage />} />
           <Route path="/resultPage" element={<ResultPage />} />
-          <Route path="/camera" element={<Camera />} />
           <Route path="/imagechoice" element={<ImageChoice />} />
           <Route path="/kidData" element={<KidData />} />
-          <Route path="/testpost" element={<Testpost />} />
         </Routes>
       </BrowserRouter>
     </>
